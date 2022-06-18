f3bf996373bdcfcaa6499cb9f85c0274676072e8
Julia Kravchenko <js.kravchenko@gmail.com>
2022-06-18 17:02:40 +0200

    The bot-file from seminar was deleted.

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
+import telebot
 from telegram import Update, Bot
 from telegram.ext import Updater, CommandHandler, Filters, MessageHandler
+from telebot import types
 
 # pip install python-telegram-bot - загрузить библиотеку
 # t.me/team_phonebook_bot -- ссылка на наш бот, pnonebook_bot - name, team_phonebook_bot
@@ -8,13 +10,15 @@ bot = Bot(token='5570893886:AAHBpHJ83y6EIx3CiNklHhMgXcZpbTaUcQs')
 updater = Updater(token='5570893886:AAHBpHJ83y6EIx3CiNklHhMgXcZpbTaUcQs', use_context=True)
 dispatcher = updater.dispatcher
 
-
+# @bot.message_handler(commands=['start'])
 def start(update, context):
     arg = context.args
     if not arg:
         context.bot.send_message(update.effective_chat.id, "Привет")
     else:
         context.bot.send_message(update.effective_chat.id, f"{' '.join(arg)}")
+
+
 
 
 def info(update, context):
@@ -33,15 +37,16 @@ def unknown(update, context):
     context.bot.send_message(update.effective_chat.id, 'Ты несешь какую-то дичь...')
 
 
-start_handler = CommandHandler('start', start)
+# start_handler = CommandHandler('start', start)
 info_handler = CommandHandler('info', info)
 message_handler = MessageHandler(Filters.text, message)
 unknown_handler = MessageHandler(Filters.command, unknown) #/game
 
-dispatcher.add_handler(start_handler)
+# dispatcher.add_handler(start_handler)
 dispatcher.add_handler(info_handler)
 dispatcher.add_handler(unknown_handler)
 dispatcher.add_handler(message_handler)
+
 
 print("server_started")
 
